ce7567347689059aec88c7f57a7546759b998cf0
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2019-08-10 20:27:25 +0200

    blak

--- bin/update_formats.py
+++ bin/update_formats.py
@@ -17,11 +17,11 @@ import stdnum
 
 
 def get_submodules(module):
-    if not module.__file__.endswith('__init__.py'):
+    if not module.__file__.endswith("__init__.py"):
         return []
     path = os.path.join(os.path.dirname(module.__file__))
     submodule_names = [
-        f'{module.__name__}.{info.name}'
+        f"{module.__name__}.{info.name}"
         for info in pkgutil.iter_modules([path])
     ]
     seen_names = set()
@@ -38,16 +38,16 @@ class Validator:
 
     @property
     def doc(self):
-        first_line = next(filter(None, self.module.__doc__.split('\n')))
-        first_line = first_line.strip('.')
+        first_line = next(filter(None, self.module.__doc__.split("\n")))
+        first_line = first_line.strip(".")
         return first_line
 
     @property
     def depth(self):
         move = 0
-        if self.relative_name.startswith('iso'):
+        if self.relative_name.startswith("iso"):
             move = -1
-        return -1 * max(1, self.name.count('.') + move)
+        return -1 * max(1, self.name.count(".") + move)
 
     @property
     def name(self):
@@ -55,12 +55,12 @@ class Validator:
 
     @property
     def relative_name(self):
-        return self.name[len('stdnum.'):]
+        return self.name[len("stdnum.") :]
 
 
 def find_validators(module):
     for submodule in get_submodules(module):
-        if hasattr(submodule, 'validate'):
+        if hasattr(submodule, "validate"):
             yield Validator(submodule)
         else:
             yield from find_validators(submodule)
@@ -68,15 +68,17 @@ def find_validators(module):
 
 def prepate_formats(validators):
     validators = list(validators)
-    validators.sort(key=operator.attrgetter('depth', 'relative_name'))
+    validators.sort(key=operator.attrgetter("depth", "relative_name"))
 
     def quote(val):
-        return val.replace('"', r'\"')
+        return val.replace('"', r"\"")
 
-    formatted = '{{\n{}\n}}'.format('\n'.join(
-        f'    "{validator.relative_name}": u"{quote(validator.doc)}",'
-        for validator in validators
-    ))
+    formatted = "{{\n{}\n}}".format(
+        "\n".join(
+            f'    "{validator.relative_name}": u"{quote(validator.doc)}",'
+            for validator in validators
+        )
+    )
     return formatted
 
 
@@ -92,10 +94,11 @@ formats = {formatted_formats}
 
 
 def write_to_file(formatted_formats):
-    target = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), '../stdnumfield/_formats.py'))
+    target = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../stdnumfield/_formats.py")
+    )
     content = TEMPLATE.format(formatted_formats=formatted_formats)
-    with open(target, 'w') as f:
+    with open(target, "w") as f:
         f.write(content)
 
 
@@ -105,5 +108,5 @@ def main():
     write_to_file(formats)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- stdnumfield/settings.py
+++ stdnumfield/settings.py
@@ -3,15 +3,13 @@ from django.conf import settings
 from stdnumfield import STDNUM_FORMATS
 
 
-_DEFAULTS = {
-    "DEFAULT_FORMATS": STDNUM_FORMATS.keys(),
-}
+_DEFAULTS = {"DEFAULT_FORMATS": STDNUM_FORMATS.keys()}
 
-_SETTINGS = getattr(settings, 'STDNUMFIELD', {})
+_SETTINGS = getattr(settings, "STDNUMFIELD", {})
 
 
 def _get(attribute):
     return _SETTINGS.get(attribute, _DEFAULTS[attribute])
 
 
-DEFAULT_FORMATS = _get('DEFAULT_FORMATS')
+DEFAULT_FORMATS = _get("DEFAULT_FORMATS")
